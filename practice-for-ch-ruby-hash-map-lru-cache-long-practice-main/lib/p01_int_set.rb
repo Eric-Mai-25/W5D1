77c242b4f56b32a4855e7ebcc989119e3781cb0b
Eric-Mai-25 <ericmai2@gmail.com>
2023-08-14 14:05:08 -0700

    implemented MaxIntSet

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
@@ -1,17 +1,26 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max){false}
   end
 
   def insert(num)
+    raise 'Out of bounds' if num > @max || num < 0
+    store[num] = true
+    return store[num] == num
   end
 
   def remove(num)
+    store[num] = false
   end
 
   def include?(num)
+    @store[num] 
   end
-
+  
+  attr_accessor :store
   private
+
 
   def is_valid?(num)
   end
